18d382025e5f9db3e60ad30618f5de7039f0e9b4
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-08-12 14:50:04 +0700

    feat: add useNavigate

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
-import { signUp } from "../features/session/sessionSlice"
+import { signUp } from "../features/session/sessionSlice";
 import { useDispatch } from "react-redux";
-// import useNavigate
+import { useNavigate } from "react-router-dom";
 
-export default function SignUp () {
+export default function SignUp() {
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const dispatch = useDispatch();
 
-   // Grab the navigate function
-
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(signUp({username: username}));
-    // imperatively redirect the user to /profile
-  }
+    dispatch(signUp({ username: username }));
+    navigate("/profile");
+  };
 
   return (
     <section>
